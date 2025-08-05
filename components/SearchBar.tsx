39c123a9571ce1998548851f37ea6ac91ad2d8d2
idealoladipupo <oladipupo53@gmail.com>
2025-08-05 12:02:36 +0100

    Removed mistaken Git repo inside project

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, TextInput, View, Image } from "react-native";
-import React from "react";
 import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
 interface props {
   placeholder: string;
   onPress?: () => void;
+  value: string;
+  onChangeText: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -18,8 +20,8 @@ const SearchBar = ({ placeholder, onPress }: props) => {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className="flex-1 ml-2 text-white"
       />
